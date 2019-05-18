9a318c743f6d3aada0c4cb1d1838188ce9ea776f
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-05-19 00:47:24 +0700

    move to authentication package

--- app/src/main/java/com/alfianyusufabdullah/chatyuk/presentation/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/alfianyusufabdullah/chatyuk/presentation/authentication/AuthenticationActivity.kt
@@ -1,17 +1,18 @@
-package com.alfianyusufabdullah.chatyuk
+package com.alfianyusufabdullah.chatyuk.presentation.authentication
 
 import android.content.Intent
 import androidx.fragment.app.FragmentTransaction
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alfianyusufabdullah.chatyuk.presentation.chatroom.ChatRoomActivity
+import com.alfianyusufabdullah.chatyuk.R
 
-import com.alfianyusufabdullah.chatyuk.fragment.FragmentLogin
-import com.alfianyusufabdullah.chatyuk.fragment.FragmentRegister
-import com.alfianyusufabdullah.chatyuk.interfaces.AuthPageListener
-import com.alfianyusufabdullah.chatyuk.model.ModelUser
-import com.alfianyusufabdullah.chatyuk.utils.ChatPreferences
+import com.alfianyusufabdullah.chatyuk.presentation.authentication.login.LoginFragment
+import com.alfianyusufabdullah.chatyuk.presentation.authentication.register.RegisterFragment
+import com.alfianyusufabdullah.chatyuk.data.entity.User
+import com.alfianyusufabdullah.chatyuk.preferences.ChatPreferences
 
-class ActivityAuth : AppCompatActivity(), AuthPageListener {
+class AuthenticationActivity : AppCompatActivity(), AuthenticationPageListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +21,7 @@ class ActivityAuth : AppCompatActivity(), AuthPageListener {
         if (savedInstanceState == null) {
             supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.containerAuth, FragmentLogin())
+                    .replace(R.id.containerAuth, LoginFragment())
                     .commit()
         }
     }
@@ -29,7 +30,7 @@ class ActivityAuth : AppCompatActivity(), AuthPageListener {
         supportFragmentManager
                 .beginTransaction()
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .replace(R.id.containerAuth, FragmentLogin())
+                .replace(R.id.containerAuth, LoginFragment())
                 .commit()
     }
 
@@ -37,13 +38,13 @@ class ActivityAuth : AppCompatActivity(), AuthPageListener {
         supportFragmentManager
                 .beginTransaction()
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .replace(R.id.containerAuth, FragmentRegister())
+                .replace(R.id.containerAuth, RegisterFragment())
                 .commit()
     }
 
-    override fun onAuthenticateSuccess(user: ModelUser) {
+    override fun onAuthenticateSuccess(user: User) {
         ChatPreferences.initPreferences(this).userInfo = user
-        startActivity(Intent(this@ActivityAuth, ActivityMain::class.java))
+        startActivity(Intent(this@AuthenticationActivity, ChatRoomActivity::class.java))
         finish()
     }
 }
