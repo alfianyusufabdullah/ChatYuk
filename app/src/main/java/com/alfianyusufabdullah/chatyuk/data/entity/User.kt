edc04445800ce16a3d6170b6752211230920e685
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-05-19 00:43:44 +0700

    move to entity package

---
+++ app/src/main/java/com/alfianyusufabdullah/chatyuk/data/entity/User.kt
@@ -0,0 +1,15 @@
+package com.alfianyusufabdullah.chatyuk.data.entity
+
+/**
+ * Created by JonesRandom on 9/15/17.
+ *
+ * @site www.androidexample.web.id
+ * @github @alfianyusufabdullah
+ */
+
+data class User(var username: String? = null,
+                var password: String? = null,
+                var confirmPassword: String? = null,
+                var email: String? = null,
+                var userId: String? = null,
+                var loginTime: String? = null)
