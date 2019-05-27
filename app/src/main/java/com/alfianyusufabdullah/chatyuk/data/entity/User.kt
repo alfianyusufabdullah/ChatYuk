8d8634929b2a14b3f75dc70be5b3de6aa3afa710
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2019-05-27 21:34:13 +0700

    delete loginTime property

--- app/src/main/java/com/alfianyusufabdullah/chatyuk/data/entity/User.kt
+++ app/src/main/java/com/alfianyusufabdullah/chatyuk/data/entity/User.kt
@@ -11,5 +11,4 @@ data class User(var username: String? = null,
                 var password: String? = null,
                 var confirmPassword: String? = null,
                 var email: String? = null,
-                var userId: String? = null,
-                var loginTime: String? = null)
+                var userId: String? = null)
